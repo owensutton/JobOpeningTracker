8c6eb7060f11d308ffcc03d0d94159daaca3b45f
owensutton <45274107+owensutton@users.noreply.github.com>
2020-08-05 22:04:24 -0500

    updates made to getting google jobs

--- Main.py
+++ Main.py
@@ -5,11 +5,22 @@
 import django
 import scrapy
 import webbrowser
+from lxml import html
 import requests
 
 def googlejobs():
-    res = requests.get('https://careers.google.com')
-    print(res)
+
+    # Start Scraping google career website
+    print ('Scraping Google Career Website')
+    # get the page request
+    page = requests.get('https://careers.google.com/jobs/results/?company=Google&company=YouTube&company=Google%20Fiber&employment_type=FULL_TIME&hl=en_US&jlo=en_US&q=&sort_by=relevance')
+
+    # Parse request into html
+    tree = html.fromstring(page.content)
+
+    #Get job titles
+    title = tree.xpath('//div[@id="search-results"]/text()')
+    print 'Title: ', title
 
 
 
