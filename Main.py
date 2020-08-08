255a8f518f3893c3d14e7ddbd91313b0b5dbf3b3
owensutton <45274107+owensutton@users.noreply.github.com>
2020-08-08 00:01:56 -0500

    adding artisan partners

--- Main.py
+++ Main.py
@@ -5,23 +5,46 @@
 import django
 import scrapy
 import webbrowser
+from bs4 import BeautifulSoup
 from lxml import html
 import requests
+import selenium
+
 
 def googlejobs():
 
     # Start Scraping google career website
     print ('Scraping Google Career Website')
+
     # get the page request
-    page = requests.get('https://careers.google.com/jobs/results/?company=Google&company=YouTube&company=Google%20Fiber&employment_type=FULL_TIME&hl=en_US&jlo=en_US&q=&sort_by=relevance')
+    url = "https://careers.google.com/jobs/results/?company=Google&company=YouTube&company=Google%20Fiber&employment_type=FULL_TIME&hl=en_US&jlo=en_US&q=&sort_by=relevance"
+    page = requests.get(url)
 
     # Parse request into html
-    tree = html.fromstring(page.content)
+    soup = BeautifulSoup(page.text)
+
 
     #Get job titles
-    title = tree.xpath('//div[@id="search-results"]/text()')
+   # title = tree.xpath('//div[@class="gc-h-unstyled-list gc-p-results__results-list"]/text()')
+    title = tree.xpath('//div/h1/p/a/span[@class="gc-h-hidden--until-medium"]/text()')
     print 'Title: ', title
 
 
+def ArtisanPartnerJobs():
 
-googlejobs()
+    print('Scraping Artisan PartnerJobs')
+
+    url = 'https://www.artisanpartners.com/careers/career-opportunities.html'
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    title = soup.find_all('title')
+    print title
+
+    h1 = soup.find('h1')
+    print h1
+
+
+
+ArtisanPartnerJobs()
